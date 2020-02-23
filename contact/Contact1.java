ab57bd5e76e30bc8685884654afd8936993debb5
fahmedaashfe <60335359+fahmedaashfe@users.noreply.github.com>
2020-02-23 21:15:18 +0600

    Add files via upload

---
+++ contact/Contact1.java
@@ -0,0 +1,10 @@
+package contact;
+
+public class Contact1 {
+	public static void main(String[] args) {
+		Contact c1 = new Contact("FAHMEDA HOQUE", "18-36767-1", 20, "01635675231", 'F');
+		c1.showPersonInfo();
+		c1.detectMobileOperator();
+	}
+
+}
