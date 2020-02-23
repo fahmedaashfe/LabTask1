f2e78138d4307ab3aa8fe40dc6ec3a70c3e55bec
fahmedaashfe <60335359+fahmedaashfe@users.noreply.github.com>
2020-02-23 19:21:03 +0600

    Add files via upload

---
+++ account/Account.java
@@ -0,0 +1,22 @@
+package account;
+
+public class Account {
+	private String accName, acid;
+	private int balance;
+	public Account() {
+		
+	}
+	public Account(String accName, String acid, int balance) {
+		this.accName = accName;
+		this.acid = acid;
+		this.balance = balance;
+	}
+	public void deposit(int amount) {
+		this.balance = this.balance + amount;
+	}
+	public void Withdraw(int amount) {
+		this.balance = this.balance - amount;
+	}
+	
+}
+
---
+++ account/Account1.java
@@ -0,0 +1,11 @@
+package account;
+
+public class Account1 {
+	public static void main(String[] args) {
+		Account a1 = new Account("FAHMEDA HOQUE", "18-36767-1", 35000);
+		a1.deposit(2200);
+
+	}
+
+}
+
