3d5df216da0104503be5b78d7fcdb55bd8fff05c
fahmedaashfe <60335359+fahmedaashfe@users.noreply.github.com>
2020-02-23 19:19:35 +0600

    Add files via upload

---
+++ book/Book1.java
@@ -0,0 +1,12 @@
+package book;
+
+public class Book1 {
+	public static void main(String[] args) {
+		Book b1 = new Book("Java The Complete Refference", "Harvert Scheild", "11-1001", "Thrilar", 13);
+		b1.showInfo();
+		Book.showTotalBookInfo();
+	}
+
+}
+
+
