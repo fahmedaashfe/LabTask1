a67ba89fcbc31dbd30e9e096c99074df152ea4dd
fahmedaashfe <60335359+fahmedaashfe@users.noreply.github.com>
2020-02-23 21:16:07 +0600

    Add files via upload

---
+++ student/Student1.java
@@ -0,0 +1,12 @@
+package student;
+
+public class Student1 {
+	public static void main(String[] args) {
+		Student s1 = new Student("FAHMEDA HOQUE", "18-36767-1", "Computer Science & Engineering", (float)2.7);
+		s1.showInfo();
+
+	}
+
+}
+
+
